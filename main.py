c5c2e275554f4a36bcf24c9323958826c9c1fff5
Deju Adejo <ojomideju2003@gmail.com>
2024-06-07 05:28:59 +0100

    Create brick layout and add ball.py

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 from turtle import Screen
 from paddle import Paddle
+from ui import Interface
 
+# Create the screen object
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title('Breakout')
 
+# Accelerate drawing
+screen.tracer(0)
+
+# Create an instance of the paddle object
 paddle = Paddle()
+
+# Create and initialize the game interface
+interface = Interface()
+interface.initialize()
 
 # Add an event listener to the screen for key presses
 screen.listen()
+# Move the left while left key is pressed
 screen.onkeypress(paddle.move_left, 'Left')
+# Move the right while right key is pressed
 screen.onkeypress(paddle.move_right, 'Right')
 
+# Show the screen after drawings have been made
+screen.update()
 screen.exitonclick()
--- ui.py
+++ ui.py
@@ -1,7 +1,30 @@
-import turtle as t
+from turtle import Turtle
 
 
-# yellow, green, orange and red
-class Interface(t.Turtle):
+class Interface(Turtle):
     def __init__(self):
+        # Initialize the super class (Turtle)
         super().__init__()
+        self.bricks = []
+        # Dictionary of brick layers and corresponding layer colors
+        self.layers = {
+            # y position: color
+            (240, 260): 'red',
+            (220, 200): 'orange',
+            (180, 160): 'green',
+            (140, 120): 'yellow'
+        }
+
+    def initialize(self):
+        # TODO: Complete the description
+        for heights, color in self.layers.items():
+            # Loop through each height position
+            for i in heights:
+                for j in range(-360, 360, 50):
+                    brick = Turtle()
+                    brick.shape('square')
+                    brick.color(color)
+                    brick.shapesize(stretch_wid=0.5, stretch_len=2)
+                    brick.penup()
+                    brick.goto(j, i)
+                    self.bricks.append(brick)
